50cbeee179dd2df86f0184c92de5afe322f0a663
christianwagstaff <=>
2022-01-16 13:33:51 -0800

    Updated Navbar to have hide/close buttons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { ReactComponent as Menu } from "../images/menu_black_24dp.svg";
+import { ReactComponent as Close } from "../images/close_black_24dp.svg";
 
 const NavBar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <nav>
-      <button className="showHideNav"></button>
-      <ul>
+      <button
+        className="showHideNav"
+        onClick={() => {
+          setToggleMenu(!toggleMenu);
+        }}
+      >
+        {toggleMenu ? <Close /> : <Menu />}
+      </button>
+      <ul className={toggleMenu ? "active" : ""}>
         <li>Home</li>
         <li>Projects</li>
         <li>About</li>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./stylesheets/reset.css";
+import "./stylesheets/index.css";
 import App from "./App";
 
 ReactDOM.render(
